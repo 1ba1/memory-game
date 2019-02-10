119d6ddc1419020c88d951ab4cd31771eacdc44e
1ba1 <vvvalenti01@yahoo.it>
2019-02-10 13:15:13 +0100

    mobile responsive + refresh button

--- index.js
+++ index.js
@@ -3,23 +3,22 @@ const images = ['\u{2600}', '\u{2600}', '\u{2615}', '\u{2615}', '\u{2622}',
   '\u{2622}', '\u{2639}', '\u{2639}', '\u{262F}', '\u{262F}', '\u{2654}',
   '\u{2654}', '\u{2693}', '\u{2693}', '\u{265C}', '\u{265C}'];
 
-function distributeImages() {
+function distributeImages() { // return random position images
   let rand = Math.round(Math.random() * 15),
     count = 0,
     iconDiv = document.createElement('div');
 
-  do {
+  while (count < 1) {
     if (images[rand] !== 0) {
       iconDiv.textContent = `${images[rand]}`;
       images[rand] = 0;
       count++;
     }
     rand = Math.round(Math.random() * 15);
-  } while (count < 1);
+  }
 
   return iconDiv;
 }
-
 
 function makeGameBoard() {
   for (let i = 0; i < 16; i++) {
@@ -31,12 +30,12 @@ function makeGameBoard() {
     divCard.appendChild(distributeImages());
     container.appendChild(divCard);
   }
+  game();
 }
 
 function game() {
   const divs = document.querySelectorAll('#container > div');
   let clickCount = 0, pair = [], guessed = [];
-
   [...divs].forEach(div => {
     div.onclick = () => {
       div.classList.add('rotate');
@@ -44,7 +43,7 @@ function game() {
       clickCount++;
       if (clickCount === 2 && pair[0] !== pair[1]) {
         [...divs].forEach(div => {
-          if (!guessed.includes(div.textContent)) {
+          if (!guessed.includes(div.textContent)) { // rotate back if no match
             setTimeout(() => {
               div.classList.remove('rotate');
             }, 700)
@@ -61,10 +60,14 @@ function game() {
       if (guessed.length === 8) {
         const p = document.querySelector('p');
         p.style.visibility = 'visible';
+        const refresh = document.querySelector('button');
+        refresh.style.display = 'inherit';
+        refresh.onclick = () => {
+          location.reload();
+        };
       }
     };
   })
 }
 
 makeGameBoard();
-game();
